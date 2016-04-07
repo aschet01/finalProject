d5a68388893975f9490fe3b055fe4b58fa7b4d0f
Adam Schettenhelm <adam.schettenhelm@gmail.com>
2016-04-06 23:07:20 -0400

    Initial commit moving prototype into Meteor

---
+++ client/js/app.js
@@ -0,0 +1,13 @@
+
+Template.body.helpers({
+  initMap: function () {
+    var myLatLng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      center: myLatLng,
+      zoom: 8,
+      // mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
+});
