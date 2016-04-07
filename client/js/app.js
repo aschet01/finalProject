e58086267831ada22108bffc97fd1cbe6bae2649
Adam Schettenhelm <adam.schettenhelm@gmail.com>
2016-04-07 10:10:43 -0400

    Add sidebar for lat/lng input

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,14 @@
+var map;
+initMap = function () {
+    var myLatLng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      center: myLatLng,
+      zoom: 8,
+      // mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
 
 Template.body.helpers({
   initMap: function () {
