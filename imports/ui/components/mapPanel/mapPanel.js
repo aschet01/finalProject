fdad45a22d9d41070090dee8d272d5397fc8636e
Adam Schettenhelm <adam.schettenhelm@gmail.com>
2016-05-09 14:06:36 -0400

    Add points of interest to style

--- imports/ui/components/mapPanel/mapPanel.js
+++ imports/ui/components/mapPanel/mapPanel.js
@@ -41,85 +41,58 @@ const mutedBlueStyle = [
   }
 ];
 
-// https://snazzymaps.com/style/25/blue-water
+// adapted https://snazzymaps.com/style/25/blue-water
 const blueWater = [
   {
-      "featureType": "administrative",
-      "elementType": "labels.text.fill",
-      "stylers": [
-          {
-              "color": "#444444"
-          }
-      ]
-  },
-  {
-      "featureType": "landscape",
-      "elementType": "all",
-      "stylers": [
-          {
-              "color": "#f2f2f2"
-          }
-      ]
-  },
-  {
-      "featureType": "poi",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "road",
-      "elementType": "all",
-      "stylers": [
-          {
-              "saturation": -100
-          },
-          {
-              "lightness": 45
-          }
-      ]
-  },
-  {
-      "featureType": "road.highway",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "simplified"
-          }
-      ]
-  },
-  {
-      "featureType": "road.arterial",
-      "elementType": "labels.icon",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "transit",
-      "elementType": "all",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "water",
-      "elementType": "all",
-      "stylers": [
-          {
-              "color": "#46bcec"
-          },
-          {
-              "visibility": "on"
-          }
-      ]
+    "featureType": "administrative",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {"color": "#444444"}
+    ]
+  },{
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+      {"color": "#f2f2f2"}
+    ]
+  },{
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [
+      {"saturation": -100},
+      {"lightness": 45}
+    ]
+  },{
+    "featureType": "road.highway",
+    "elementType": "all",
+    "stylers": [
+      {"visibility": "simplified"}
+    ]
+  },{
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+      {"color": "#46bcec"},
+      {"visibility": "on"}
+    ]
+  // },{
+  //   "featureType": "poi",
+  //   "elementType": "all",
+  //   "stylers": [
+  //     {"visibility": "off"}
+  //   ]
+  // },{
+  //   "featureType": "road.arterial",
+  //   "elementType": "labels.icon",
+  //   "stylers": [
+  //     {"visibility": "off"}
+  //   ]
+  // },{
+  //   "featureType": "transit",
+  //   "elementType": "labels.icon",
+  //   "stylers": [
+  //     {"visibility": "off"}
+  //   ]
   }
 ];
 
